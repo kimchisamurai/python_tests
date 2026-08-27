2d73a41b731e23ef33b053be2821fd826a4bb18d
Hlib <94254569+kimchisamurai@users.noreply.github.com>
2026-08-27 11:21:03 +0300

    Implement Caesar cipher encryption

---
+++ caesarcipher.py
@@ -0,0 +1,8 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+shift = 5
+shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+translation_table = str.maketrans(alphabet, shifted_alphabet)
+text = 'hello world'
+encrypted_text = text.translate(translation_table)
+
+ 
