97a1664cb06644a6d3dc187f7ba72de571649efc
Hlib <94254569+kimchisamurai@users.noreply.github.com>
2026-08-28 05:06:23 +0300

    Add caesar function for Caesar cipher encryption

--- caesarcipher.py
+++ caesarcipher.py
@@ -5,6 +5,7 @@ translation_table = str.maketrans(alphabet, shifted_alphabet)
 text = 'hello world'
 encrypted_text = text.translate(translation_table)
 
+
 def caesar(text, shift):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
